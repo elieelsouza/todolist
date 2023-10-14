6098742b0bbe540747aadcbbc2f65532f3d134b6
Eliel de Souza Santos <eliel.santos.dev@gmail.com>
2023-10-14 15:51:50 -0300

    Adicionando H2 Database para tempo de desenvolvimento

--- src/main/java/br/com/elielsouza/todolist/user/UserModel.java
+++ src/main/java/br/com/elielsouza/todolist/user/UserModel.java
@@ -1,12 +1,27 @@
 package br.com.elielsouza.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import lombok.Data;
 
 @Data
+@Entity(name= "tb_users")
 public class UserModel {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String name;
     private String username;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     
 }
